807a7681a53aa811ef745642a3323a89031dc351
huongvu <huongse7en>
2024-02-01 11:05:43 +0700

    [Task] Update for u2u nebulas

--- apps/evm/src/app/swap/header.tsx
+++ apps/evm/src/app/swap/header.tsx
@@ -6,8 +6,6 @@ import React, { FC } from 'react'
 import { SUPPORTED_CHAIN_IDS } from 'src/config'
 
 export const Header: FC = () => {
-  console.log("🚀 ~ SUPPORTED_CHAIN_IDS:", SUPPORTED_CHAIN_IDS)
-
   return (
     <Navigation
       rightElement={<WagmiHeaderComponents chainIds={SUPPORTED_CHAIN_IDS} />}
